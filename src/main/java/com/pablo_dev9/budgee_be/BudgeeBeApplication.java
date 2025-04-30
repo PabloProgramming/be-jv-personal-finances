f680e6c4641cc0b2a92622eb95b90bafe3c702d9
Pablo <pmontalvoster@gmail.com>
2025-04-30 17:16:11 +0100

    inital commit

---
+++ src/main/java/com/pablo_dev9/budgee_be/BudgeeBeApplication.java
@@ -0,0 +1,13 @@
+package com.pablo_dev9.budgee_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgeeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgeeBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pablo_dev9/budgee_be/BudgeeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablo_dev9.budgee_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgeeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
